38f086220be6432dcf63dae6c8116624381bdeb4
Michael Carratt <mcarratt@gmail.com>
2013-09-26 02:46:49 -0700

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Nomicator::Application.routes.draw do
-  get "static_pages/home"
-
-  get "static_pages/help"
-
-  get "static_pages/about"
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  root to: 'static_pages/home', action: 'get'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,9 +51,7 @@ Nomicator::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+
 
   # See how all your routes lay out with "rake routes"
 
