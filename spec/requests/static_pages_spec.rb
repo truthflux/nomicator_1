60bef47ac995271cf312bd4cf76f9481493232ff
Michael Carratt <mcarratt@gmail.com>
2013-09-26 00:08:41 -0700

    refactoring titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,14 +2,18 @@ require 'spec_helper'
 
 describe "Static pages" do
     describe "Home page" do
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Nomicator | Home")
+      expect(page).to have_title("Nomicator")
     end
 
     it "should have the content 'Nomicator'" do
       visit '/static_pages/home'
       expect(page).to have_content('Nomicator')
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
