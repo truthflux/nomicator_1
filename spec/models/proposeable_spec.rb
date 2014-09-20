a74f81531fbbb9e5eb32daef7317eb9bbeb95a80
Truthflux <truthflux@gmail.com>
2014-09-20 16:38:42 -0400

    Remove dodgy specs before re-scope

--- spec/models/proposeable_spec.rb
+++ spec/models/proposeable_spec.rb
@@ -1,30 +1,5 @@
 require 'spec_helper'
 
 describe Proposeable do
-  let (:user) { FactoryGirl.create(:user) }
-
-  before do
-    @proposeable = user.proposeables.build( title: "Lorem ipsum", content: "Lorem ipsum",
-                                            ruleset_id: 1, proposeable_type_id: 1)
-  end
-
-  #TODO proposeable factory
-  #TODO add in use of proposeable_type factory
-
-  subject { @proposeable }
-
-  it { should respond_to(:title) }
-  it { should respond_to(:content) }
-  it { should respond_to(:resolve_time) }
-  it { should respond_to(:version_number) }
-  it { should respond_to(:ruleset_id) }
-  it { should respond_to(:proposeable_type_id) }
-  it { should respond_to(:successor_id) }
-  it { should respond_to(:proposer_id) }
-  it { should respond_to(:resolver_id) }
-
-  it { should be_valid }
-
-
-
+  pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/models/proposeable_type_spec.rb
+++ spec/models/proposeable_type_spec.rb
@@ -1,29 +1,6 @@
 require 'spec_helper'
 
 describe ProposeableType do
-
-before do
-
- let (:proposeable_type) { FactoryGirl.create(:proposeable_type) }
-
-end
-
-subject { :proposeable_type }
-
-it { should respond_to(:type_name) }
-it { should respond_to(:interval) }
-it { should respond_to(:weekday) }
-it { should respond_to(:voter_role_id) }
-it { should respond_to(:instant_resolve) }
-it { should respond_to(:majority) }
-it { should respond_to(:quorum) }
-it { should respond_to(:in_effect) }
-it { should respond_to(:succeeded) }
-it { should respond_to(:archived) }
-it { should respond_to(:actionable) }
-it { should respond_to(:resolution_type_id) }
-
-
-it { should be_valid }
+  pending "add some examples to (or delete) #{__FILE__}"
 
 end
